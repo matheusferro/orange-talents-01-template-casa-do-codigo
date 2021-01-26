d4d67a8906efbf5ccfd21ff732d3ee48a89eba0a
matheus.ferro <matheussouzaferro@gmail.com>
2021-01-26 10:16:29 -0300

    0.0.2 - CADASTRO AUTOR COM VALIDACAO.

---
+++ src/main/java/br/com/zup/casaDoCodigo/autor/AutorController.java
@@ -0,0 +1,20 @@
+package br.com.zup.casaDoCodigo.autor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/autor")
+public class AutorController {
+
+    @Autowired
+    private AutorRepository autorRepository;
+
+    @PostMapping
+    public String cadastrar(@RequestBody @Valid AutorForm form){
+        autorRepository.save(form.converterToAutor());
+        return "Autor cadastrado.";
+    }
+}
---
+++ src/main/java/br/com/zup/casaDoCodigo/autor/AutorForm.java
@@ -0,0 +1,48 @@
+package br.com.zup.casaDoCodigo.autor;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class AutorForm {
+
+    @NotBlank
+    private String nome;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Length(max=400)
+    private String descricao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Autor converterToAutor(){
+        return new Autor(nome, email, descricao);
+    }
+}
